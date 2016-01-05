267e576b8956bf9dec062ee5f827a3a8c4778c35
Charles Vaneenoo <charlesvaneenoo@MacBook-Air-de-Charles.local>
2016-01-05 22:59:21 +0100

    first implementation of the class Player

--- BombermanProject/src/main/java/players/Player.java
+++ BombermanProject/src/main/java/players/Player.java
@@ -1,7 +1,51 @@
 package players;
 
+import java.awt.Point;
+
+import gameframework.drawing.DrawableImage;
+import gameframework.drawing.GameCanvas;
+import gameframework.drawing.SpriteManagerDefaultImpl;
+import gameframework.game.GameData;
+import gameframework.motion.MoveStrategy;
+import gameframework.motion.MoveStrategyKeyboard;
+
 public class Player {
 	
-	//TO - DO : Carlito ou Pierre
+	protected SpriteManagerDefaultImpl spriteManager;
+	protected int spriteSize;
+	protected GameCanvas canvas;
+	protected GameData data;
+	protected Point direction;
 
+
+
+
+
+	/**
+	 * 
+	 * @param x initial p of our player
+	 * @param y initial position of our player
+	 */
+	public Player(int x, int y){
+		this.data = data;
+		this.canvas = data.getCanvas(); //bomberman on the canvas
+		this.spriteSize = data.getConfiguration().getSpriteSize();// size of bomberman
+		this.spriteManager = new SpriteManagerDefaultImpl(new DrawableImage("/resources/finalGifIcons/BombermanBas.gif", canvas), this.spriteSize, 3); //give the gif of bomberman
+		this.direction = new Point(0, 1); // direction of bomberman
+		//this.setLocation(x, y); // start position of bomberman doesn't work .. don't know why
+
+	}
+	
+	
+	
+	
+	/**
+	 * 
+	 * @param pos
+	 * @param goal
+	 * @return
+	 */
+	protected MoveStrategy getMoveStrategy(Point pos, Point goal) {
+		return new MoveStrategyKeyboard(); 
+	}
 }
