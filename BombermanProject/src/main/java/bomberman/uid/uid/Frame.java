283300507e1cc94093a2bc6b6a151acdd2460732
NVKFTW <kevingallet@gmail.com>
2016-01-05 14:53:04 +0100

    See last commit

--- BombermanProject/src/main/java/bomberman/uid/uid/Frame.java
+++ BombermanProject/src/main/java/bomberman/uid/uid/Frame.java
@@ -8,13 +8,16 @@ import gameframework.gui.GameWindow;
 public class Frame {
 
 	
-	 public static void main(String[] args){
+	 public Frame(){
 		 GameConfiguration config = new GameConfiguration();
 		 GameData data = new GameData(config);
 		 GameCanvasDefaultImpl canvas = new GameCanvasDefaultImpl();
 		 GameWindow windows = new GameWindow("BombermanProject",canvas,config);
-		 
 		 windows.createGUI();
+	 }
+	 
+	 public static void main(String[] args){
+		 Frame frm = new Frame();
 	 }
 	
 }
