8f8d89a6c4818e6b80409fefcf5aa2ada441e774
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-10 00:19:27 +0100

    creation des tests

---
+++ BombermanProject/src/test/java/uid/WallTest.java
@@ -0,0 +1,29 @@
+package uid;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import gameframework.game.GameConfiguration;
+import gameframework.game.GameData;
+
+public class WallTest {
+	
+	GameConfiguration myGameConfiguration;
+	GameData data;
+	Wall myWall;
+	
+	@Before
+	public void create(){
+		GameConfiguration myGameConfiguration = new GameConfiguration(20, 20, 20, 20);
+		GameData data = new GameData(myGameConfiguration);
+		Wall myWall = new Wall(data,20,20);
+	}
+	
+	@Test
+	public void test(){
+	}
+	
+	
+}
