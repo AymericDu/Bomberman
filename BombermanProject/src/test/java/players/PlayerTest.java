263a08247c8de0423f4ceff8cb96bcb08633b186
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-10 00:18:53 +0100

    creation des tests

---
+++ BombermanProject/src/test/java/players/PlayerTest.java
@@ -0,0 +1,17 @@
+package players;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	Player myPlayer;
+	
+	@Test
+	public void test() {
+		myPlayer = new Player(0,0);
+		fail("Not yet implemented");
+	}
+
+}
