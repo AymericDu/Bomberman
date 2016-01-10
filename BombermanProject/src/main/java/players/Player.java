efe5d5ddde3eed2c9257d0bdc417e28a21083d6d
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-10 12:34:40 +0100

    add function, comment, change constructor

--- BombermanProject/src/main/java/players/Player.java
+++ BombermanProject/src/main/java/players/Player.java
@@ -1,15 +1,17 @@
 package players;
 
 import java.awt.Point;
+import java.awt.Rectangle;
 
 import gameframework.drawing.DrawableImage;
 import gameframework.drawing.GameCanvas;
 import gameframework.drawing.SpriteManagerDefaultImpl;
 import gameframework.game.GameData;
+import gameframework.motion.GameMovable;
 import gameframework.motion.MoveStrategy;
 import gameframework.motion.MoveStrategyKeyboard;
 
-public class Player {
+public class Player extends GameMovable {
 	
 	protected SpriteManagerDefaultImpl spriteManager;
 	protected int spriteSize;
@@ -24,7 +26,7 @@ public class Player {
 	 * @param x initial p of our player
 	 * @param y initial position of our player
 	 */
-	public Player(int x, int y){
+	public Player(GameData data, int x, int y){
 		this.data = data;
 		this.canvas = data.getCanvas(); //bomberman on the canvas
 		this.spriteSize = data.getConfiguration().getSpriteSize();// size of bomberman
@@ -41,5 +43,48 @@ public class Player {
 	protected MoveStrategy getMoveStrategy() {
 		return new MoveStrategyKeyboard(); 
 	}
+
+	/**
+	 * return the rectangle which represent the game space
+	 */
+	@Override
+	public Rectangle getBoundingBox() {
+		Rectangle rectangle = new Rectangle(this.spriteSize, this.spriteSize);
+		rectangle.setLocation(position.x * this.spriteSize, position.y* this.spriteSize);
+		return rectangle;	
+		}
+
+	
+	/**
+	 * add sprite change if our player move
+	 **/
+	@Override
+	public void oneStepMoveAddedBehavior() {
+		Point d = this.moveDriver.getSpeedVector(this).getDirection();
+		if (!direction.equals(d)) {
+			// player go right
+			if (d.equals(new Point(1, 0))) {
+				this.spriteManager.setType("right");
+				direction = d;
+			// player go left
+			} else if (d.equals(new Point(-1, 0))) {
+				this.spriteManager.setType("left");
+				direction = d;
+			// player go on top
+			} else if (d.equals(new Point(0, -1))) {
+				this.spriteManager.setType("up");
+				direction = d;
+			// player go on bottom
+			} else if (d.equals(new Point(0, 1))) {
+				this.spriteManager.setType("down");
+				direction = d;
+			}
+		}
+		// player don't move 
+		if (!(d.equals(new Point(0, 0)))) {
+			this.spriteManager.increment();
+		}
+	}		
+	
 	
 }
