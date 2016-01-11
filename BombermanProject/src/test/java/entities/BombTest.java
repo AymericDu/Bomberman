9afa20313056b78d9cef4ce50008e2d635e1cc5b
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-11 11:48:39 +0100

    Creation of BombTest class

---
+++ BombermanProject/src/test/java/entities/BombTest.java
@@ -0,0 +1,14 @@
+package entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BombTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
