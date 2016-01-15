66b8fb2c839a49cafbc27ac2611920cb1e87deda
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-15 15:23:20 +0100

    rename Frame.java in Main.java and modify the constructor

--- src/uid/Main.java
+++ src/uid/Main.java
@@ -5,18 +5,17 @@ import gameframework.game.GameConfiguration;
 import gameframework.game.GameData;
 import gameframework.gui.GameWindow;
 
-public class Frame {
+public class Main {
 
-	public Frame() {
-		GameConfiguration config = new GameConfiguration();
-		GameData data = new GameData(config);
+	public Main() {
+		GameConfiguration config = new GameConfiguration(40, 40, 0, 1);
 		GameCanvasDefaultImpl canvas = new GameCanvasDefaultImpl();
 		GameWindow windows = new GameWindow("Bomberman", canvas, config);
 		windows.createGUI();
 	}
 
 	public static void main(String[] args) {
-		Frame frm = new Frame();
+		new Main();
 	}
 
 }
