f81df6f1e06f8a03dbb2bd0803e28c4690fb505a
NVKFTW <kevingallet@gmail.com>
2016-01-15 16:32:09 +0100

    Added main song for the game

--- src/uid/Main.java
+++ src/uid/Main.java
@@ -2,28 +2,39 @@ package uid;
 
 import java.applet.Applet;
 import java.applet.AudioClip;
+import java.awt.Component;
 import java.net.URL;
 
+import entity.Box;
+import gameframework.drawing.BackgroundImage;
+import gameframework.drawing.DrawableImage;
 import gameframework.drawing.GameCanvasDefaultImpl;
 import gameframework.game.GameConfiguration;
+import gameframework.game.GameData;
 import gameframework.gui.GameWindow;
 
 public class Main {
 
+	URL url = Main.class.getResource("/sounds/GameSound.wav");
+	AudioClip clip = Applet.newAudioClip(url);
+	
 	public Main() {
 		GameConfiguration config = new GameConfiguration(40, 40, 0, 1);
 		GameCanvasDefaultImpl canvas = new GameCanvasDefaultImpl();
 		GameWindow windows = new GameWindow("Bomberman", canvas, config);
 		windows.createGUI();
+		this.startSong();
+	}
+	
+	public void startSong(){
+		clip.play();
+	}
+	
+	public void stopSong(){
+		clip.stop();
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		new Main();
-		
-		URL url = Main.class.getResource("/sounds/GameSound.wav");
-		AudioClip clip = Applet.newAudioClip(url);
-		clip.play();
-		Thread.sleep(1000);
-		clip.stop();
+		new Main();	
 	}
 }
