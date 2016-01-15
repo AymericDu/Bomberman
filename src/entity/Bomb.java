be9b63c636bd207ab3dd0594cb62780514846f34
NVKFTW <kevingallet@gmail.com>
2016-01-15 14:54:12 +0100

    Added  gif for explosion, added modifications BombExplosion, Bomb and BombTest

--- src/entity/Bomb.java
+++ src/entity/Bomb.java
@@ -1,19 +1,66 @@
 package entity;
 
-public class Bomb {
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
-	protected int x;
-	protected int y;
-	protected int power;
+import gameframework.drawing.Drawable;
+import gameframework.drawing.DrawableImage;
+import gameframework.drawing.GameCanvas;
+import gameframework.game.GameData;
+import gameframework.game.GameEntity;
+import gameframework.motion.blocking.MoveBlocker;
 
-	public Bomb(int x, int y, int power) {
-		// ou alors un Point p en parametre a voir
-		this.x = x;
-		this.y = y;
-		this.power = power;
+public class Bomb implements GameEntity,  MoveBlocker, Drawable, ActionListener {
+
+	protected Timer timer;
+	protected GameData data;
+	protected GameCanvas canvas;
+	protected DrawableImage img;
+	protected Point position;
+	protected int radius;
+
+	public Bomb(GameData data, int x, int y, int radius) {
+		this.data = data;
+		this.canvas = data.getCanvas();
+		this.position = new Point(x,y);
+		this.radius = radius;
+		this.img = new DrawableImage("/images/level1/caisselvl1.gif",this.canvas);
+		this.timer = new Timer();
 	}
 
-	public int getPower() {
-		return this.power;
+	public int getRadius() {
+		return this.radius;
+	}
+
+	@Override
+	public Rectangle getBoundingBox() {
+		Rectangle rectangle = new Rectangle(this.img.getWidth(), this.img.getHeight());
+		rectangle.setLocation(position.x, position.y);		
+		return rectangle;
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		this.canvas.drawImage(g, this.img.getImage(), this.position.x, this.position.y);
+	}
+
+	@Override
+	public boolean isMovable() {
+		return false;
+	}
+	
+	public void startTimer(){
+		//BombExplosion task = new BombExplosion(this.data, this.radius);
+		//timer.schedule(task, 2000);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 	}
 }
