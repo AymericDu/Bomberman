a097d6015d6f4438da1d875bed7d1f99b3d1e4b2
Pierre Hequette <hequette.pierre@gmail.com>
2016-01-18 23:20:14 +0100

    Add of sound for bombExplosion + methods related to it and update of jdb

--- src/main/java/bomberman/entity/BombExplosion.java
+++ src/main/java/bomberman/entity/BombExplosion.java
@@ -1,27 +1,55 @@
 package bomberman.entity;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 import javax.swing.Timer;
 
+import bomberman.uid.Main;
 import gameframework.game.GameData;
 
 public class BombExplosion extends NoMovableEntity implements ActionListener {
 
 	protected Timer timer;
+	protected AudioClip clip;
+	protected boolean exploded;
 
 	public BombExplosion(GameData data, Point position) {
 		super(data, position, "/images/level/Explode.png");
 		this.timer = new Timer(1000, this);
 		this.timer.setRepeats(false);
 		this.timer.start();
+		this.exploded = false;
+		URL url = Main.class.getResource("/sounds/ExplosionSound.wav");
+		this.clip = Applet.newAudioClip(url);
 		// TODO kill player and destroy box
 	}
 
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO remove the bomb explosion
+		this.exploded = true;
+		startSound();
+	}
+	
+	public boolean bombIsExploded(Bomb b){
+		if (this.exploded == true){
+			return true;
+		}
+		else 
+			return false;
+	}
+	
+	public void startSound(){
+		this.clip.play();
+	}
+	
+	public void stopSong() {
+		this.clip.stop();
 	}
 }
