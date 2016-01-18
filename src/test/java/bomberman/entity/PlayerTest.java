5469bd8c7f0618bbffb91ef7b264f01fa04c2c85
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-18 19:54:31 +0100

    continue the test

--- src/test/java/bomberman/entity/PlayerTest.java
+++ src/test/java/bomberman/entity/PlayerTest.java
@@ -2,8 +2,9 @@ package bomberman.entity;
 
 import static org.junit.Assert.*;
 
-import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,12 +19,15 @@ public class PlayerTest {
 	GameConfiguration myGameConfiguration;
 	GameData data;
 	Player playerTest;
-
+	List<Bomb> bombsAvailable;
+	
 	@Before
 	public void create(){
 		myGameConfiguration = new GameConfiguration(20, 20, 20, 20);
 		data = new GameData(myGameConfiguration);
 		playerTest = new Player(data, new Point(4, 4));
+		bombsAvailable = new ArrayList<Bomb>();
+
 	}
 
 	@Test
@@ -49,4 +53,29 @@ public class PlayerTest {
 		assertEquals(true, playerTest.isMovable());
 	}
 	
+	@Test
+	public void dropBombTest(){
+		playerTest.dropBomb();
+		assertEquals(true, this.bombsAvailable.isEmpty());
+		this.bombsAvailable.add(new Bomb(this.data, playerTest.getPosition(), 2));
+		assertNotNull(bombsAvailable);
+		playerTest.dropBomb();
+		assertEquals(false, this.bombsAvailable.isEmpty());
+	}
+	
+	@Test
+	public void getSpriteManagerTest(){
+		playerTest.initSpriteManager();
+		assertNotNull(playerTest.getSpriteManager());
+	}
+	
+	/*
+	@Test
+	public void cha(){
+		Point p = new Point(1,0);
+		playerTest.setPosition(p);
+		playerTest.initSpriteManager();
+		playerTest.changeSpriteDirection();
+		assertEquals("right", playerTest.getSpriteManager());
+	}*/
 }
