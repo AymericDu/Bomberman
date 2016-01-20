3df82cc4bd4462299386786e8536b6cb2ae91b3b
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-20 14:35:50 +0100

    remove methods bombIsExploded and the boolean exploded

--- src/main/java/bomberman/entity/BombExplosion.java
+++ src/main/java/bomberman/entity/BombExplosion.java
@@ -16,33 +16,21 @@ public class BombExplosion extends NoMovableEntity implements ActionListener {
 
 	protected Timer timer;
 	protected AudioClip clip;
-	protected boolean exploded;
 
 	public BombExplosion(GameData data, Point position) {
 		super(data, position, "/images/level/Explode.png");
 		this.timer = new Timer(1000, this);
 		this.timer.setRepeats(false);
 		this.timer.start();
-		this.exploded = false;
 		URL url = Main.class.getResource("/sounds/ExplosionSound.wav");
 		this.clip = Applet.newAudioClip(url);
 		// TODO kill player and destroy box
 	}
 
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO remove the bomb explosion
-		this.exploded = true;
 		startSound();
-	}
-	
-	public boolean bombIsExploded(Bomb b){
-		if (this.exploded == true){
-			return true;
-		}
-		else 
-			return false;
 	}
 	
 	public void startSound(){
