3f8ef35b19f562291e50cc18a8db1b2c25b02600
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-21 20:55:15 +0100

    refactoring of test class with an @Before and adding test for getPosition

--- src/test/java/bomberman/entity/NoMovableEntityTest.java
+++ src/test/java/bomberman/entity/NoMovableEntityTest.java
@@ -1,9 +1,10 @@
 package bomberman.entity;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 import java.awt.Point;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import gameframework.game.GameConfiguration;
@@ -11,18 +12,29 @@ import gameframework.game.GameData;
 
 public abstract class NoMovableEntityTest {
 
-	GameConfiguration myGameConfiguration = new GameConfiguration(20, 20, 20, 20);;
-	GameData data = new GameData(myGameConfiguration);
-	Point position = new Point(0, 0);
+	GameConfiguration myGameConfiguration;
+	GameData data;
+	Point position;
+	NoMovableEntity entity;
+	
+	@Before
+	public void create(){
+		myGameConfiguration = new GameConfiguration(20, 20, 20, 20);;
+		data = new GameData(myGameConfiguration);
+		position = new Point(0, 0);
+		entity = this.createNoMovableEntity();
 
-	/**
-	 * use the attributes
-	 */
+	}
+	
 	public abstract NoMovableEntity createNoMovableEntity();
-
+	
 	@Test
 	public void noMovableTest() {
-		NoMovableEntity entity = this.createNoMovableEntity();
 		assertFalse(entity.isMovable());
+	}
+	
+	@Test
+	public void getPositionTest(){
+		assertEquals(position ,entity.getPosition());
 	}
 }
