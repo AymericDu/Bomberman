0d36e1c5bbb4d0394ee6da0a00540e9e836c5340
AymericDu <aymeric.ducroquetz@etudiant.univ-lille1.fr>
2016-01-22 22:06:00 +0100

    refactor Player

--- src/main/java/bomberman/entity/Player.java
+++ src/main/java/bomberman/entity/Player.java
@@ -3,31 +3,16 @@ package bomberman.entity;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.net.URL;
 
 import bomberman.uid.BombermanMoveStrategy;
-import gameframework.drawing.DrawableImage;
-import gameframework.drawing.GameCanvas;
 import gameframework.drawing.SpriteManagerDefaultImpl;
 import gameframework.game.GameData;
-import gameframework.game.GameEntity;
-import gameframework.motion.GameMovable;
-import gameframework.motion.MoveStrategy;
-import gameframework.motion.MoveStrategyConfigurableKeyboard;
-import gameframework.motion.MoveStrategyKeyboard;
-import gameframework.motion.overlapping.Overlappable;
 
-public class Player extends GameMovable implements GameEntity, Overlappable {
+public class Player extends Entity {
 
 	protected SpriteManagerDefaultImpl spriteManager;
-	protected int spriteSize;
-	protected GameCanvas canvas;
-	protected GameData data;
-	protected Point direction;
-	protected boolean isAlive;
 	protected int authorizedBombs;
 	protected BombermanMoveStrategy keyboard;
-	protected URL url;
 	
 	/**
 	 * Constructor of player class, allow to create our player
@@ -38,23 +23,49 @@ public class Player extends GameMovable implements GameEntity, Overlappable {
 	 *            initial position of our player
 	 */
 	public Player(GameData data, Point position) {
-		this.data = data;
-		this.canvas = data.getCanvas();
-		this.spriteSize = data.getConfiguration().getSpriteSize();
-		url = Player.class.getResource("/images/level/BombermanSprite.png");
-		this.spriteManager = new SpriteManagerDefaultImpl(new DrawableImage(url, canvas), this.spriteSize, 5);
-
-		this.isAlive = true;
-		this.authorizedBombs = 1;
-
-		this.setPosition(position);
-		this.direction = new Point(0, 1);
-		this.initSpriteManager();
-
+		super(data, position, "/images/level/BombermanSprite.png");
 
 		this.getDriver().setmoveBlockerChecker(data.getMoveBlockerChecker());
 
-		this.data.getUniverse().addGameEntity(this);
+		this.spriteManager = new SpriteManagerDefaultImpl(this.img, data.getConfiguration().getSpriteSize(), 5);
+		this.spriteManager.setTypes("down", "right", "up", "left", "died");
+		this.spriteManager.setType("down");
+		this.spriteManager.reset();
+
+		this.authorizedBombs = 1;
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		this.spriteManager.draw(g, position);
+	}
+
+	@Override
+	public Rectangle getBoundingBox() {
+		int spriteSize = data.getConfiguration().getSpriteSize();
+		Rectangle rectangle = new Rectangle(spriteSize, spriteSize);
+		rectangle.setLocation((int) this.position.getX() * spriteSize, (int) this.position.getY() * spriteSize);
+		return rectangle;
+	}
+
+	/**
+	 * do one step of a movement
+	 */
+	@Override
+	public void oneStepMoveAddedBehavior() {
+		Point direction = this.moveDriver.getSpeedVector(this).getDirection();
+		if (direction.equals(new Point(1, 0))) {
+			this.spriteManager.setType("right");
+		} else if (direction.equals(new Point(-1, 0))) {
+			this.spriteManager.setType("left");
+		} else if (direction.equals(new Point(0, -1))) {
+			this.spriteManager.setType("up");
+		} else if (direction.equals(new Point(0, 1))) {
+			this.spriteManager.setType("down");
+		}
+		if (!(direction.equals(new Point(0, 0)))) {
+			this.spriteManager.increment();
+		}
 	}
 
 	public void setKeyboard(BombermanMoveStrategy keyboard) {
@@ -63,60 +74,6 @@ public class Player extends GameMovable implements GameEntity, Overlappable {
 		this.keyboard.setSpeed(this.data.getConfiguration().getSpriteSize());
 		this.getDriver().setStrategy(keyboard);
 		this.data.getCanvas().addKeyListener(keyboard);
-	}
-
-	/**
-	 * MoveStrategy allow to define the strategy choose by the player
-	 * 
-	 * @return the choosen strategy
-	 */
-	protected MoveStrategy getMoveStrategy() {
-		return new MoveStrategyKeyboard();
-	}
-
-	/**
-	 * getGameData allows to give the GameData of our player
-	 */
-	public GameData getGameData() {
-		return this.data;
-	}
-
-	/**
-	 * setDirection allows to change the direction of our player
-	 * 
-	 * @param p
-	 *            the new direction
-	 */
-	public void setDirection(Point p) {
-		this.direction = p;
-	}
-
-	/**
-	 * getDirection return the direction of our player
-	 * 
-	 * @return the direction
-	 */
-	public Point getDirection() {
-		return this.direction;
-	}
-	
-	/**
-	 * getKeyboard return the MoveStrategyKeyboard of our player
-	 * @return the MoveStrategyKeyboard
-	 */
-	public MoveStrategyConfigurableKeyboard getKeyboard() {
-		return this.keyboard;
-	}
-
-	/**
-	 * return the rectangle which represent the game space
-	 */
-	@Override
-	public Rectangle getBoundingBox() {
-		Rectangle rectangle = new Rectangle(this.spriteSize, this.spriteSize);
-		rectangle.setLocation((int) this.position.getX() * this.spriteSize,
-				(int) this.position.getY() * this.spriteSize);
-		return rectangle;
 	}
 
 	/**
@@ -137,94 +94,11 @@ public class Player extends GameMovable implements GameEntity, Overlappable {
 	}
 
 	/**
-	 * getAuthorizedBombs allows to have the number of bombs you can drop
-	 * @return the authorizedBombs
-	 */
-	public int getAuthorizedBombs(){
-		return this.authorizedBombs;
-	}
-	
-	/**
-	 * setAuthorizedBombs allows to change the number of authorized bombs
-	 */
-	public void setAuthorizedBombs(int nb){
-		this.authorizedBombs = nb;
-	}
-	
-	/**
-	 * @return true if the player is alive and false if he's dead
-	 */
-	protected boolean getIsAlive() {
-		return isAlive;
-	}
-
-	/**
 	 * This function allows to change isAlive in false. It allow to kill the
 	 * player.
 	 */
 	public void kill() {
-		// TODO Change img
-		this.isAlive = false;
 		this.spriteManager.setType("died");
 		this.data.getCanvas().removeKeyListener(this.keyboard);
-	}
-
-	/**
-	 * Initialize the spriteManager with a picture composed by 5 animations
-	 */
-	public void initSpriteManager() {
-		this.spriteManager.setTypes("down", "right", "up", "left", "died");
-		this.spriteManager.setType("down");
-		this.spriteManager.reset();
-	}
-
-	/**
-	 * @return the sprite manager
-	 */
-	public SpriteManagerDefaultImpl getSpriteManager() {
-		return this.spriteManager;
-	}
-
-	/**
-	 * draw the player with the Graphics g, the image and the coordinates of
-	 * him.
-	 * 
-	 * @param g
-	 *            : Graphics
-	 */
-	@Override
-	public void draw(Graphics g) {
-		this.spriteManager.draw(g, position);
-	}
-
-	/**
-	 * do one step of a movement
-	 */
-	@Override
-	public void oneStepMoveAddedBehavior() {
-		Point d = this.moveDriver.getSpeedVector(this).getDirection();
-		if (!direction.equals(d)) {
-			if (d.equals(new Point(1, 0))) {
-				this.spriteManager.setType("right");
-				direction = d;
-			} else if (d.equals(new Point(-1, 0))) {
-				this.spriteManager.setType("left");
-				direction = d;
-			} else if (d.equals(new Point(0, -1))) {
-				this.spriteManager.setType("up");
-				direction = d;
-			} else if (d.equals(new Point(0, 1))) {
-				this.spriteManager.setType("down");
-				direction = d;
-			}
-		}
-		if (!(d.equals(new Point(0, 0)))) {
-			this.spriteManager.increment();
-		}
-	}
-
-	@Override
-	public boolean isMovable() {
-		return true;
 	}
 }
