3bc9fbd7dcad91ce01245d96908401f216b80e1f
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-22 18:13:14 +0100

    fix FlameTest bug in URL

--- src/test/java/bomberman/entity/FlameTest.java
+++ src/test/java/bomberman/entity/FlameTest.java
@@ -4,6 +4,6 @@ public class FlameTest extends NoMovableEntityTest {
 
 	@Override
 	public Flame createNoMovableEntity() {
-		return new Flame(this.data, this.position,"/images/level/ExplodeDown.png");
+		return new Flame(this.data, this.position,"/images/level/flameCenter32.png");
 	}
 }
