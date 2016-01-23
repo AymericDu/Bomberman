126e96f0504974b4c3e34e2e1a77d6d425875bb7
AymericDu <aymeric.ducroquetz@etudiant.univ-lille1.fr>
2016-01-23 22:20:09 +0100

    fix bug removeAllEntities

--- src/main/java/bomberman/uid/BombermanUniverse.java
+++ src/main/java/bomberman/uid/BombermanUniverse.java
@@ -1,10 +1,21 @@
 package bomberman.uid;
 
+import gameframework.game.GameEntity;
 import gameframework.game.GameUniverseDefaultImpl;
+import gameframework.motion.blocking.MoveBlocker;
+import gameframework.motion.overlapping.Overlappable;
 
 public class BombermanUniverse extends GameUniverseDefaultImpl {
 
 	public synchronized void removeAllEntities() {
+		for (GameEntity gameEntity : this.gameEntities) {
+			if (gameEntity instanceof Overlappable) {
+				getOverlapProcessor().removeOverlappable((Overlappable) gameEntity);
+			}
+			if (gameEntity instanceof MoveBlocker) {
+				getMoveBlockerChecker().removeMoveBlocker((MoveBlocker) gameEntity);
+			}
+		}
 		this.gameEntities.clear();
 	}
 }
