89415c8609d0fa837b4b06c7c3dfa6121b97d632
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-23 11:06:52 +0100

    adding test for BombBonusTest and create class + test for BombRadiusBonusTest

--- src/test/java/bomberman/entity/bonus/BombRadiusBonusTest.java
+++ src/test/java/bomberman/entity/bonus/BombRadiusBonusTest.java
@@ -1,30 +1,30 @@
 package bomberman.entity.bonus;
 
+import static org.junit.Assert.*;
+
 import java.awt.Point;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 import bomberman.entity.Player;
 import gameframework.game.GameConfiguration;
 import gameframework.game.GameData;
 
-public class BombBonusTest extends BonusTest {
+public class BombRadiusBonusTest extends BonusTest {
 
 	@Override
-	public BombBonus createEntity() {
-		return new BombBonus(this.data, this.position);
+	public Bonus createEntity() {
+		return new BombRadiusBonus(this.data, this.position);
 	}
-
+	
 	@Test
 	public void effectTest() {
 		GameConfiguration conf = new GameConfiguration();
 		GameData data = new GameData(conf);
 		Bonus bonus = this.createEntity();
 		Player player = new Player(data, new Point(1,1));
-		assertEquals(1, player.getAuthorizedBomb());
+		assertEquals(1, player.getBombsRadius());
 		bonus.effect(player);
-		assertEquals(2, player.getAuthorizedBomb());
+		assertEquals(2, player.getBombsRadius());
 	}
 }
