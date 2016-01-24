0a59cf0367c27e2e55661918b90304ac3166fc6a
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-24 10:05:14 +0100

    correct broken test constructor in FlameTest

--- src/test/java/bomberman/entity/explosion/FlameTest.java
+++ src/test/java/bomberman/entity/explosion/FlameTest.java
@@ -1,11 +1,13 @@
 package bomberman.entity.explosion;
 
+import java.awt.Point;
+
 import bomberman.entity.EntityTest;
 
 public class FlameTest extends EntityTest {
 
 	@Override
 	public Flame createEntity() {
-		return new Flame(this.data, this.position, "/images/explosion/FlameCenter.png");
+		return new Flame(this.data, this.position, "/images/explosion/FlameCenter.png", new Point(1,1));
 	}
 }
