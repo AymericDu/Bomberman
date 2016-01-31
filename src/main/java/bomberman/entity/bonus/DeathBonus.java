d3084a0af554f7f2550d96248227645a56fb02f8
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-31 20:42:47 +0100

    adding comments and correct for all the bonus

--- src/main/java/bomberman/entity/bonus/DeathBonus.java
+++ src/main/java/bomberman/entity/bonus/DeathBonus.java
@@ -5,25 +5,26 @@ import java.awt.Point;
 import bomberman.entity.player.Player;
 import gameframework.game.GameData;
 
-
 /**
- * Death bonus, bonus which kill the player when he cross it
- *
+ * This class represents the Death bonus, bonus which kill the player when he cross on it
  */
 public class DeathBonus extends Bonus {
 
 	/**
 	 * Constructor of the death bonus
-	 * @param data the game's data
-	 * @param position the position of the entity
-	 * @param urlString the string representing the path to the entity's image
+	 * @param data 
+	 * 				: the game's data
+	 * @param position 
+	 * 				: the position of the entity
+	 * @param urlString 
+	 * 				: the string representing the path to the entity's image
 	 */
 	public DeathBonus(GameData data, Point position) {
 		super(data, position, "/images/bonus/DeathBonus.png");
 	}
 	
 	/**
-	 * the bonus will kill whoever pick it(or step on it)
+	 * the bonus will kill whoever pick it (or step on it)
 	 */
 	@Override
 	public void effect(Player player) {
@@ -31,4 +32,3 @@ public class DeathBonus extends Bonus {
 		super.effect(player);
 	}
 }
-
